eae29df884b738e753970877e67495a1cef52f8b
Caleb Carvalho <you@example.com>
2016-11-18 12:01:37 +0100

    add delete handler

--- assets/js/gosite.js
+++ assets/js/gosite.js
@@ -1,8 +1,17 @@
-console.log("my scripts is running");
 //http://classify.oclc.org/classify2/Classify?&summary=true&title=
 //	templates := template.Must(template.ParseFiles("templates/base.html", "templates/web-go/index.html"))
 //= template "templates/base.html"
 
+function deleteBook(pk){
+  console.log("Deteling ", pk);
+  $.ajax({
+    method:"GET",
+    url: "/books/delete?pk="+pk,
+    success: function(){
+      $("#book-row-"+ pk).remove();
+    }
+  })
+}
 function showSearchPage(){
   $("#search-page").show();
   $("#view-page").hide();
@@ -12,19 +21,18 @@ function showViewPage(){
   $("#view-page").show();
 }
 function submitSearch(){
-  console.log("inside submit function");
   $.ajax({
     url:"/search",
     method: "POST",
     data: $("#search-form").serialize(),
     success: function(rawData){
-      console.log(rawData);
       var parsed = JSON.parse(rawData);
       if (!parsed)return
       var SearchResults = $("#search-results");
       SearchResults.empty();
       parsed.forEach(function(result){
-        var row = $("<tr><td>"+ result.Title + "</td><td>" + result.Author + "</td><td>" + result.Year + "</td><td>"+ result.ID + "</td></tr>");
+        var row = $("<tr><td>"+ result.Title + "</td><td>" + result.Author + "</td><td>" + result.Year +
+        "</td><td>"+ result.ID + "</td></tr>");
         SearchResults.append(row);
         row.on("click", function(){
           $.ajax({
@@ -33,7 +41,10 @@ function submitSearch(){
             success: function(data){
               var book = JSON.parse(data)
               if (!book)return;
-              $("#view-results").append("<tr><td>"+ result.Title + "</td><td>" + result.Author + "</td><td>" + result.Classification + "</td></tr>");
+              $("#view-results").append("<tr id='book-row-"+ book.PK +"'><td>"+ book.Title +
+              "</td><td>" + book.Author + "</td><td>" +
+               book.Classification +
+               "</td><td><button class='delete-btn' onclick='deleteBook(" + book.PK + ")'>Delete</button></td></tr>");
             }
           })
         })
